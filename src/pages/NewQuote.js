cfba6dd989c9c2d011f1976290ce61d8e0086d0c
insung <96061695+kisn3089@users.noreply.github.com>
2022-07-28 00:18:42 +0900

    connet firebase

--- src/pages/NewQuote.js
+++ src/pages/NewQuote.js
@@ -1,18 +1,26 @@
+import { useEffect } from 'react';
 import { useHistory } from 'react-router-dom';
 import React from 'react'
 import QuoteForm from '../components/quotes/QuoteForm';
+import useHttp from '../hooks/use-http';
+import { addQuote } from '../lib/api';
 
 const NewQuote = () => {
+  const {sendRequest, status} = useHttp(addQuote);
     const history = useHistory();
 
-    const addQuoteHandler = quoteData => {
-        console.log(quoteData);
-
+    useEffect(() => {
+      if(status === 'completed') {
         history.push('/quotes');
+      }
+    }, [status, history])
+
+    const addQuoteHandler = quoteData => {
+        sendRequest(quoteData);
     }
 
   return (
-    <QuoteForm onAddQuote={addQuoteHandler} />
+    <QuoteForm isLoading={status === 'pending'} onAddQuote={addQuoteHandler} />
   )
 }
 
